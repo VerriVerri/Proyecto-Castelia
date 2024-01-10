9036fb82d4f0104eb3dd99f932bfcac242e1bb42
Robin\PCHOME <vikbm928@gmail.com>
2024-01-10 23:37:55 +0100

    Commit del menu de Inicio con el de settings y el de pause.

--- Assets/Scripts/Controls/PlayerController.cs
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    // Update is called once per frame
+public class Player : MonoBehaviour
+{
+    public GameObject animatorObj;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    bool isGrounded;
+    bool isJumping;
+
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            // Jump
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+
+        if (!isGrounded)
+        {
+            //Player is airborne
+            animatorObj.GetComponent<Animator>().Play("jump");
+        }
+
+        if (horizontalInput == 0)
+        {
+            // Player is idle
+            Debug.Log("standing still");
+
+            if (isGrounded)
+            {
+                animatorObj.GetComponent<Animator>().Play("idle");
+            }
+        }
+
+        if (horizontalInput < 0)
+        {
+            // Player is moving left
+            Debug.Log("Moving left");
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
+            if (isGrounded)
+            {
+                animatorObj.GetComponent<Animator>().Play("walk");
+            }
+        }
+        else if (horizontalInput > 0)
+        {
+            // Player is moving right
+            Debug.Log("Moving right");
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+            if (isGrounded)
+            {
+                animatorObj.GetComponent<Animator>().Play("walk");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
